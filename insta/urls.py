011559f03592369ee54b6e14e2ab0194add63813
kmunge <orokomunge@gmail.com>
2019-06-25 15:04:45 +0300

    define logoutview

--- insta/urls.py
+++ insta/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
-from django.contrib.auth import views
+from django.contrib.auth import LogoutView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('posts.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
-    url(r'^logout/$', views.logout, {"next_page": '/'}),
+    url(r'^logout/$', LogoutView.logout, {"next_page": '/'}),
     url(r'^tinymce/', include('tinymce.urls')),
 ]
