e44edb0be72eca5583a0c8aa0d4dbcaa1e48a775
kmunge <orokomunge@gmail.com>
2019-06-25 16:18:31 +0300

    adding form to collect user uploads

--- posts/forms.py
+++ posts/forms.py
@@ -4,15 +4,14 @@ from .models import Image, Comments, Profile
 class NewPostForm(forms.ModelForm):
     class Meta:
         model = Image
-        exclude = ['profile', 'publish_date','poster_id']
+        exclude = ['profile','publish_date','poster_id']
 
 
 class NewCommentForm(forms.ModelForm):
     class Meta:
         model = Comments
-        exclude = ['date_posted', 'author', 'post_id', 'image']
+        exclude = ['date_posted','author', 'post_id', 'image']
 
-   
 class NewProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
